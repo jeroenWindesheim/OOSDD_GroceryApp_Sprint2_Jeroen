0cc85dc0c35a27abc7342a78c9d9177343184216
jeroenWindesheim <S1215501@student.windesheim.nl>
2025-09-17 12:51:16 +0200

    You can add a product in the grocery list if they are in the available list. If a product is added, the stock and available list are updated.

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -68,12 +68,28 @@ namespace Grocery.App.ViewModels
         [RelayCommand]
         public void AddProduct(Product product)
         {
-            //Controleer of het product bestaat en dat de Id > 0
-            //Maak een GroceryListItem met Id 0 en vul de juiste productid en grocerylistid
-            //Voeg het GroceryListItem toe aan de dataset middels de _groceryListItemsService
-            //Werk de voorraad (Stock) van het product bij en zorg dat deze wordt vastgelegd (middels _productService)
-            //Werk de lijst AvailableProducts bij, want dit product is niet meer beschikbaar
-            //call OnGroceryListChanged(GroceryList);
+            // Controleer of het object bestaat en dat de Id > 0
+            if (product != null && product.Id > 0)
+            {
+                // Maak een GroceryListItem met Id 0 en vul de juiste productid en grocerylistid
+                GroceryListItem groceryItem = new GroceryListItem(0, GroceryList.Id, product.Id, 1);
+                
+                // Voeg het GroceryListItem toe aan de dataset middels de _groceryListItemsService
+                _groceryListItemsService.Add(groceryItem);
+                
+                // Werk de voorraad (Stock) van het product bij en zorg dat deze wordt vastgelegd (middels _productService)
+                product.Stock -= 1;
+                _productService.Update(product);
+                
+                // Werk de lijst AvailableProducts bij, want dit product is niet meer beschikbaar
+                GetAvailableProducts();
+                
+                // call OnGroceryListChanged(GroceryList);
+                OnGroceryListChanged(GroceryList);
+            }
+
+            // GroceryListItemsService();
+
         }
     }
 }
